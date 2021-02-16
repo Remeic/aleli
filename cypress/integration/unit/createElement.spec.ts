f4da2bb63eb34b441142cc09673d3f41b53ba2b6
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-02-16 15:38:36 +0100

    Test, createElement return VNode with props property that contains children Children is Array of allowed types, contains also primitive value

--- cypress/integration/unit/createElement.spec.ts
+++ cypress/integration/unit/createElement.spec.ts
@@ -1,15 +1,24 @@
-import { create } from 'cypress/types/lodash';
-import {createElement, VNode} from '@src/createElement';
+import { create } from "cypress/types/lodash";
+import { createElement, VNode } from "@src/createElement";
 
-describe('Testing createElement function, it return a vnode', () => {
-    it("createElement return object with type and other property empty", () => {
-        const expectedVNode: VNode = {type: 'div', props: {}}
-        expect(createElement("div",{},[])).to.deep.equal(expectedVNode)
-    })
+describe("Testing createElement function, it return a vnode", () => {
+  it("createElement return VNode with type and other property empty", () => {
+    const expectedVNode: VNode = { type: "div", props: { children: [] } };
+    expect(createElement("div", {})).to.deep.equal(expectedVNode);
+  });
 
-    it("createElement return object with type and props", () => {
-        const props: Object = {name: 'Alelí'}
-        const expectedVNode: VNode = {type: 'div', props}
-        expect(createElement("div",props,[])).to.deep.equal(expectedVNode)
-    })
-})
+  it("createElement return VNode with type and props", () => {
+    const props: Object = { name: "Alelí", children: [] };
+    const expectedVNode: VNode = { type: "div", props };
+    expect(createElement("div", props)).to.deep.equal(expectedVNode);
+  });
+
+  it("createElement return VNode where children is array that contain a primitive", () => {
+    const expectedVNode: VNode = {
+      type: "div",
+      props: { children: ["Hello"] },
+    };
+    expect(createElement("div", {}, "Hello")).to.deep.equal(expectedVNode);
+  });
+  
+});
