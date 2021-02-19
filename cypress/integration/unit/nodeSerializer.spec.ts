a5608963f6c7a83d7e4adc761bfa3327f5b3c7f3
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-02-19 22:58:28 +0100

    Test, add test to check attribute serialization, refactor " to '

--- cypress/integration/unit/nodeSerializer.spec.ts
+++ cypress/integration/unit/nodeSerializer.spec.ts
@@ -1,19 +1,26 @@
-import {DomTreeSerializer,DomTreeStringify} from "@utils/DomTreeSerializer"
+import {DomTreeSerializer,DomTreeStringify} from '@utils/DomTreeSerializer'
 
-describe("Stringify DOM tree", () => {
+describe('Stringify DOM tree', () => {
   let domTreeStringify: DomTreeSerializer = new DomTreeStringify();
 
-  it("serialize plain div", () => {
-    const element: HTMLElement = document.createElement("div");
-    expect(domTreeStringify.serializeNode(element)).to.equal("<div></div>");
+  it('serialize plain div', () => {
+    const element: HTMLElement = document.createElement('div');
+    expect(domTreeStringify.serializeNode(element)).to.equal('<div></div>');
+  });
+
+  it('serialize plain div with class attribute and id', () => {
+    const element: HTMLElement = document.createElement('div');
+    element.className = 'test Alelí'
+    element.id="id"
+    expect(domTreeStringify.serializeNode(element)).to.equal('<div class="test Alelí" id="id"></div>');
   });
   
-  it("serialize text node", () => {
-    const element: HTMLElement = document.createElement("div");
-    const textElement: Text = new Text("Alelí");
+  it('serialize text node', () => {
+    const element: HTMLElement = document.createElement('div');
+    const textElement: Text = new Text('Alelí');
     element.appendChild(textElement);
     expect(domTreeStringify.serializeNode(element)).to.equal(
-      "<div>Alelí</div>"
+      '<div>Alelí</div>'
     );
   });
 });
