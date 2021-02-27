ee382b27e6eaf69353a65980ff9bc50b322d8931
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-02-27 23:08:56 +0100

    render methods should render HTMLElement with user difeined attribute

--- test/unit/render/render.spec.ts
+++ test/unit/render/render.spec.ts
@@ -1,30 +1,55 @@
+import { VNode } from "@src/types/vnode";
+import AleliRenderer from "@src/render";
+import {
+  DomTreeSerializer,
+  DomTreeStringify,
+} from "@src/utils/DomTreeSerializer";
+import { Renderer } from "@src/types/renderer";
 
-import {VNode} from "@src/types/vnode";
-import AleliRenderer from "@src/render"
-import {DomTreeSerializer,DomTreeStringify} from '@src/utils/DomTreeSerializer'
-import {Renderer} from "@src/types/renderer"
-
-describe('Testing render function, it render VNode', () => {  
-  let serializer: DomTreeSerializer
-  let aleliRenderer: Renderer
+describe("Testing render function, it render VNode", () => {
+  let serializer: DomTreeSerializer;
+  let aleliRenderer: Renderer;
 
   beforeAll(() => {
-    serializer = new DomTreeStringify()
-    aleliRenderer = new AleliRenderer()
-  })
-
-  it('render should render empty div', () => {
-    let vnode: VNode = { type: "div", props: {} }
-    let root : HTMLElement = document.createElement("div")
-    aleliRenderer.render(vnode, root)
-    expect(serializer.serializeNode(root)).toEqual("<div><div></div></div>")
+    serializer = new DomTreeStringify();
+    aleliRenderer = new AleliRenderer();
   });
 
-  it('render should render div with attribute like class', () => {
-    let vnode: VNode<{className: "Alelí"}> = { type: "div", props: { className: "Alelí", children: [""] }}
-    let root : HTMLElement = document.createElement("div")
-    aleliRenderer.render(vnode, root)
-    expect(serializer.serializeNode(root)).toEqual(`<div><div class="Alelí"></div></div>`)
+  it("render should render empty div", () => {
+    let vnode: VNode = {
+      type: "div",
+      props: {
+        children: [],
+      },
+    };
+    let root: HTMLElement = document.createElement("div");
+    aleliRenderer.render(vnode, root);
+    expect(serializer.serializeNode(root)).toEqual("<div><div></div></div>");
   });
+
+  it("render should render div with attribute like class", () => {
+    let vnode: VNode<{ className: "Alelí" }> = {
+      type: "div",
+      props: { className: "Alelí", children: [""] },
+    };
+    let root: HTMLElement = document.createElement("div");
+    aleliRenderer.render(vnode, root);
+    expect(serializer.serializeNode(root)).toEqual(
+      `<div><div class="Alelí"></div></div>`
+    );
+  });
+
+  it("render should render div with user defined attribute", () => {
+    let vnode: VNode<{ name: "Alelí" }> = {
+      type: "div",
+      props: { name: "Alelí", children: [""] },
+    };
+    let root: HTMLElement = document.createElement("div");
+    aleliRenderer.render(vnode, root);
+    expect(serializer.serializeNode(root)).toEqual(
+      `<div><div name="Alelí"></div></div>`
+    );
+  });
+
   
-})
+});
