914d7e0be3cfc6d16446a7d10b6117f09e389af2
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-02-27 23:22:07 +0100

    Feat, render method render VNode child and Text Node child

--- src/render/render.ts
+++ src/render/render.ts
@@ -10,7 +10,15 @@ export default class AleliRenderer implements Renderer{
         Object.keys(vnode.props).filter(this.isNotChildrenProp).forEach(prop => {
             if(prop === 'className') htmlElement.className = vnode.props['className'] as string
             else htmlElement.setAttribute(prop,vnode.props[prop] as string)
-        }) 
+        })
+        for(let children of vnode.props.children as Array<Children>) {
+            if(typeof(children) === 'object'){
+                this.render(children as VNode, htmlElement)
+            }
+            else if(typeof(children) === 'string'){
+                htmlElement.appendChild(document.createTextNode(children as string))
+            }
+        }
         root.appendChild(htmlElement)
     }
 
