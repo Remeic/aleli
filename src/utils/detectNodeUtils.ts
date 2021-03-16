167de474107df4937ee71dd39d45d3d94eedacd8
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-03-16 19:48:05 +0100

    Test & Refactor, remove some mutator related to specific issue StringLiterals are disabled in this moment, type:'' is used to respect VNode type setAttribute is spyed to ensure is called only if prop value change

--- src/utils/detectNodeUtils.ts
+++ src/utils/detectNodeUtils.ts
@@ -5,5 +5,5 @@ export  function isVNode(node: Children) {
   return node && typeof node === "object" && "props" in node;
 }
 export  function isNotTextNode(node: VNode) {
-  return node && typeof node === "object" && node.type !== '$TEXT'
+  return isVNode(node) && node.type !== '$TEXT'
 }
