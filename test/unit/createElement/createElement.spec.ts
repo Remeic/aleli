c87948b8e606e7d3154442ecae7ba29cc27ffefb
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-04-05 20:02:31 +0200

    Fix, AComponent doesn't implement mounting method

--- test/unit/createElement/createElement.spec.ts
+++ test/unit/createElement/createElement.spec.ts
@@ -46,6 +46,9 @@ describe("Testing createElement function, it return a vnode", () => {
 
 describe('Testing createElement function when class components are used', () => {
   class AComponent extends AleliComponent{
+    mounting(): void {
+      throw new Error("Method not implemented.");
+    }
     constructor(){super()}
     render(): VNode<{}> {
       return {
