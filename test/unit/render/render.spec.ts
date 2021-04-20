2eee5b803ea37edeb42873c92d689f3664d330fd
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-04-20 16:08:12 +0200

    Test, Renderer Method findOldChildrenIfExists should return empty if old child is not found or the old vnode

--- test/unit/render/render.spec.ts
+++ test/unit/render/render.spec.ts
@@ -20,44 +20,52 @@ describe("AleliRenderer method setProperty", () => {
 
   it("AleliRenderer method setProperty should throw error if prop key is not in props", () => {
     const htmlElement = document.createElement("div");
-    const propName : string = "color"
+    const propName: string = "color";
     const props: VNode["props"] = {
       id: 1,
       children: [],
     };
-    expect(() => aleliRenderer.setProperty(htmlElement, propName, props)).toThrowError(`Error setProperty: prop ${propName} is not present in props`)
+    expect(() =>
+      aleliRenderer.setProperty(htmlElement, propName, props)
+    ).toThrowError(
+      `Error setProperty: prop ${propName} is not present in props`
+    );
   });
 
   it("AleliRenderer method setProperty should throw error if prop key is not in props", () => {
     const htmlElement = document.createElement("div");
-    const propName : string = "color"
+    const propName: string = "color";
     const props: VNode["props"] = {
       id: 1,
       children: [],
     };
-    expect( () => aleliRenderer.setProperty(htmlElement, propName, props)).toThrowError(`Error setProperty: prop ${propName} is not present in props`)
+    expect(() =>
+      aleliRenderer.setProperty(htmlElement, propName, props)
+    ).toThrowError(
+      `Error setProperty: prop ${propName} is not present in props`
+    );
   });
 
   it("AleliRenderer method setProperty should set event listener", () => {
-    const mocked : Function = () => {}
+    const mocked: Function = () => {};
     let props: VNode["props"] = {
-        onClick:  mocked,
-        children: [],
-    }
+      onClick: mocked,
+      children: [],
+    };
     const htmlElement = document.createElement("div");
-    aleliRenderer.setProperty(htmlElement, 'onClick', props);
-    expect(htmlElement).toHaveProperty('onclick')
-    expect(typeof htmlElement.onclick!).toBe('function')
+    aleliRenderer.setProperty(htmlElement, "onClick", props);
+    expect(htmlElement).toHaveProperty("onclick");
+    expect(typeof htmlElement.onclick!).toBe("function");
   });
 
   it("AleliRenderer method setProperty should not set key prop", () => {
     let props: VNode["props"] = {
-        key:  "1",
-        children: [],
-    }
+      key: "1",
+      children: [],
+    };
     const htmlElement = document.createElement("div");
-    aleliRenderer.setProperty(htmlElement, 'key', props);
-    expect(htmlElement).not.toHaveProperty('key')
+    aleliRenderer.setProperty(htmlElement, "key", props);
+    expect(htmlElement).not.toHaveProperty("key");
   });
 });
 
@@ -189,62 +197,65 @@ describe("AleliRenderer method should call setProperty only when needed", () => 
   });
 });
 
-describe('AleliRenderer method removeProperty', () => {
+describe("AleliRenderer method removeProperty", () => {
   let aleliRenderer: AleliRenderer;
 
   beforeAll(() => {
     aleliRenderer = new AleliRenderer();
   });
 
-  it('AleliRenderer method removeProperty should remove attribute from HTMLElement', () => {
+  it("AleliRenderer method removeProperty should remove attribute from HTMLElement", () => {
     const htmlElement = document.createElement("div");
-    htmlElement.setAttribute("id","test")
-    aleliRenderer.removeProperty(htmlElement,"id")
-    expect(htmlElement.attributes).not.toHaveProperty("id")
+    htmlElement.setAttribute("id", "test");
+    aleliRenderer.removeProperty(htmlElement, "id");
+    expect(htmlElement.attributes).not.toHaveProperty("id");
   });
 
-  it('AleliRenderer method removeProperty should not throw error if prop is not inside HTMLElement', () => {
+  it("AleliRenderer method removeProperty should not throw error if prop is not inside HTMLElement", () => {
     const htmlElement = document.createElement("div");
-    expect(() => aleliRenderer.removeProperty(htmlElement,"id")).not.toThrowError()
+    expect(() =>
+      aleliRenderer.removeProperty(htmlElement, "id")
+    ).not.toThrowError();
   });
 
-  it('AleliRenderer method removeProperty should remove class instead of className', () => {
+  it("AleliRenderer method removeProperty should remove class instead of className", () => {
     const htmlElement = document.createElement("div");
-    htmlElement.classList.add("testClass")
-    aleliRenderer.removeProperty(htmlElement,'className')
-    expect(htmlElement.attributes).not.toHaveProperty('class')
+    htmlElement.classList.add("testClass");
+    aleliRenderer.removeProperty(htmlElement, "className");
+    expect(htmlElement.attributes).not.toHaveProperty("class");
   });
 
-  it('AleliRenderer method removeProperty should remove event listener', () => {
+  it("AleliRenderer method removeProperty should remove event listener", () => {
     const htmlElement = document.createElement("div");
-    htmlElement.onclick = () => {}
-    expect(htmlElement).toHaveProperty('onclick')
-    aleliRenderer.removeProperty(htmlElement,'onClick')
-    expect(htmlElement).toHaveProperty('onclick')
-    expect(typeof htmlElement.onclick!).toBe("object")
+    htmlElement.onclick = () => {};
+    expect(htmlElement).toHaveProperty("onclick");
+    aleliRenderer.removeProperty(htmlElement, "onClick");
+    expect(htmlElement).toHaveProperty("onclick");
+    expect(typeof htmlElement.onclick!).toBe("object");
   });
+});
 
-})
-
-describe('AleliRenderer method createElement', () => {
+describe("AleliRenderer method createElement", () => {
   let aleliRenderer: AleliRenderer;
 
   beforeAll(() => {
     aleliRenderer = new AleliRenderer();
-  })
+  });
 
-  it('AleliRenderer method createElement should return DOM HTMLElement if type is not $TEXT', () => {
+  it("AleliRenderer method createElement should return DOM HTMLElement if type is not $TEXT", () => {
     let vnode: VNode = {
       type: "div",
       props: {
         children: [],
       },
     };
-    const htmlElement : HTMLElement = aleliRenderer.createElement(vnode) as HTMLElement
-    expect(htmlElement.localName).toBe("div")
+    const htmlElement: HTMLElement = aleliRenderer.createElement(
+      vnode
+    ) as HTMLElement;
+    expect(htmlElement.localName).toBe("div");
   });
 
-  it('AleliRenderer method createElement should return Text Node if type is $TEXT', () => {
+  it("AleliRenderer method createElement should return Text Node if type is $TEXT", () => {
     let vnode: VNode = {
       type: "$TEXT",
       props: {
@@ -252,10 +263,9 @@ describe('AleliRenderer method createElement', () => {
         children: [],
       },
     };
-    const textNode : Text = document.createTextNode("Alelí")
-    expect(aleliRenderer.createElement(vnode)).toStrictEqual(textNode)
+    const textNode: Text = document.createTextNode("Alelí");
+    expect(aleliRenderer.createElement(vnode)).toStrictEqual(textNode);
   });
-
 });
 
 describe("AleliRenderer method removeOldChild", () => {
@@ -311,41 +321,220 @@ describe("AleliRenderer method findOldChildrenIfExists", () => {
     aleliRenderer = new AleliRenderer();
   });
 
-  it("AleliRenderer method removeOldChildren should remove Dom node", () => {
-    const parent: HTMLElement = document.createElement("div");
-    const child: HTMLElement = document.createElement("div");
-    parent.appendChild(child);
-    expect(parent.firstChild).toBe(child);
-    let vnode: VNode = {
-      type: parent.localName,
+  it("AleliRenderer method findOldChildrenIfExists should return empty vnode if old child is not found", () => {
+    const emtpyVNode: VNode = {
+      type: "",
       props: {
         children: [],
       },
-      dom: child,
     };
-    aleliRenderer.removeOldChild(vnode);
-    expect(parent.firstChild).toBe(null);
-  });
 
-  it("AleliRenderer method removeOldChildren should not do anything if oldNode does not have dom prop", () => {
-    const parent: HTMLElement = document.createElement("div");
-    const child: HTMLElement = document.createElement("div");
-    parent.appendChild(child);
-    expect(parent.firstChild).toBe(child);
-    let vnode: VNode = {
-      type: parent.localName,
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        key: 1,
+        children: [],
+      },
+    };
+
+    const vnode: VNode = {
+      type: "div",
       props: {
         children: [
-          {
-            type: child.localName,
-            props: {
-              children: [],
-            },
-          },
+          oldVNode
         ],
       },
     };
-    aleliRenderer.removeOldChild(vnode);
-    expect(parent.firstChild).toBe(child);
+
+    const newChildVNode: VNode = {
+      type: "span",
+      props: {
+        children: [],
+      },
+    };
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, newChildVNode, 1)
+    ).toStrictEqual(emtpyVNode);
   });
+
+  it("AleliRenderer method findOldChildrenIfExists, if key exists and is the same but type differ should render empty vnode", () => {
+    const emtpyVNode: VNode = {
+      type: "",
+      props: {
+        children: [],
+      },
+    };
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        key: 1,
+        children: [],
+      },
+    };
+    const vnode: VNode = {
+      type: "div",
+      props: {
+        children: [oldVNode],
+      },
+    };
+    const newChildVNode: VNode = {
+      type: "span",
+      props: {
+        children: [],
+      },
+    };
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, newChildVNode, 1)
+    ).toStrictEqual(emtpyVNode);
+  });
+
+  it("AleliRenderer method findOldChildrenIfExists, if key exists with same value and type is the same, should return old vnode", () => {
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        key: 1,
+        children: [],
+      },
+    };
+
+    const vnode: VNode = {
+      type: "div",
+      props: {
+        children: [oldVNode],
+      },
+    };
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, oldVNode, 1)
+    ).toStrictEqual(oldVNode);
+  });
+
+  it("AleliRenderer method findOldChildrenIfExists, if key exists in new VNode and not in the old one, should return emtpy vnode", () => {
+    const emtpyVNode: VNode = {
+      type: "",
+      props: {
+        children: [],
+      },
+    };
+
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        children: [],
+      },
+    };
+
+    const vnode: VNode = {
+      type: "div",
+      props: {
+        children: [oldVNode],
+      },
+    };
+
+    const newChildVNode: VNode = {
+      type: "h1",
+      props: {
+        key: 1,
+        children: [],
+      },
+    };
+
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, newChildVNode, 1)
+    ).toStrictEqual(emtpyVNode);
+  });
+
+  it("AleliRenderer method findOldChildrenIfExists, if key exists in old VNode and not in the new one, should return emtpy vnode", () => {
+    const emtpyVNode: VNode = {
+      type: "",
+      props: {
+        children: [],
+      },
+    };
+
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        key: 1,
+        children: [],
+      },
+    };
+
+    const vnode: VNode = {
+      type: "div",
+      props: {
+        children: [oldVNode],
+      },
+    };
+
+    const newChildVNode: VNode = {
+      type: "h1",
+      props: {
+        children: [],
+      },
+    };
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, newChildVNode, 1)
+    ).toStrictEqual(emtpyVNode);
+  });
+
+  it("AleliRenderer method findOldChildrenIfExists, if key not exists but type and position are the same return old vnode", () => {
+
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        children: [],
+      },
+    };
+
+    const vnode: VNode = {
+      type: "div",
+      props: {
+        children: [oldVNode],
+      },
+    };
+
+    const newChildVNode: VNode = {
+      type: "h1",
+      props: {
+        children: [],
+      },
+    };
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, newChildVNode, 0)
+    ).toStrictEqual(oldVNode);
+  });
+
+  it("AleliRenderer method findOldChildrenIfExists, if key not exists but type is the same and position is different return empty vnode", () => {
+    const emtpyVNode: VNode = {
+      type: "",
+      props: {
+        children: [],
+      },
+    };
+
+    const oldVNode: VNode = {
+      type: "h1",
+      props: {
+        children: [],
+      },
+    };
+
+    const vnode: VNode = {
+      type: "div",
+      props: {
+        children: [oldVNode],
+      },
+    };
+
+    const newChildVNode: VNode = {
+      type: "h1",
+      props: {
+        children: [],
+      },
+    };
+    expect(
+      aleliRenderer.findOldChildrenIfExists(vnode, newChildVNode, 1)
+    ).toStrictEqual(emtpyVNode);
+  });
+
 });
