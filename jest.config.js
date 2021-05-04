651a3cdfcb000759f2be2f23556a81e83ed65944
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-05-04 21:25:28 +0200

    Jest config, remove .test folder from ignore pattern

--- jest.config.js
+++ jest.config.js
@@ -175,7 +175,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  "testPathIgnorePatterns": [".test", "node_modules"],
+  "testPathIgnorePatterns": ["node_modules"],
 
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
