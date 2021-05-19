01f1c7be295266e87ef78ed4c43a47628e48e05a
Giulio Fagioli <fagioli.giulio@gmail.com>
2021-05-19 22:55:42 +0200

    Test, E2E test with some basic behaviour Minor fixes to createElement function due to issue with map operation on array

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import AleliComponent from "./components";
+import createElement from "./createElement";
+import AleliRenderer from "./renderer";
+
+export  {AleliRenderer,AleliComponent, createElement}
